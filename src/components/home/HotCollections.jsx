75ea1cd75c12f3ee85f8a8d06e668776dd4d9b12
Muhammad Faizan Amjad <Mfaizanamjad21@gmail.com>
2026-09-09 17:45:46 -0400

    Fix project and prepare deployment

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,111 +1,79 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Slider from "react-slick";
-
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
 
-const HotCollections = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 600,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    pauseOnHover: true,
-    arrows: true,
-
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
-
+const NewItems = () => {
   return (
-    <section id="section-collections" className="no-bottom">
+    <section id="section-items" className="no-bottom">
       <div className="container">
         <div className="row">
-          {/* Section Title */}
           <div className="col-lg-12">
-            <div className="text-center anim-fade-up">
-              <h2>Hot Collections</h2>
+            <div className="text-center">
+              <h2>New Items</h2>
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
+          {new Array(4).fill(0).map((_, index) => (
+            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+              <div className="nft__item">
+                <div className="author_list_pp">
+                  <Link
+                    to="/author"
+                    data-bs-toggle="tooltip"
+                    data-bs-placement="top"
+                    title="Creator: Monica Lucas"
+                  >
+                    <img className="lazy" src={AuthorImage} alt="" />
+                    <i className="fa fa-check"></i>
+                  </Link>
+                </div>
+                <div className="de_countdown">5h 30m 32s</div>
 
-          {/* Collections Carousel */}
-          <div className="col-lg-12">
-            <Slider {...settings}>
-              {new Array(4).fill(0).map((_, index) => (
-                <div
-                  key={index}
-                  className={`px-2 anim-fade-up delay-${
-                    (index % 6) + 1
-                  }`}
-                >
-                  <div className="nft_coll">
-                    {/* Collection Image */}
-                    <div className="nft_wrap">
-                      <Link to="/item-details">
-                        <img
-                          src={nftImage}
-                          className="lazy img-fluid"
-                          alt="Pinky Ocean NFT"
-                        />
-                      </Link>
-                    </div>
-
-                    {/* Creator */}
-                    <div className="nft_coll_pp">
-                      <Link to="/author">
-                        <img
-                          className="lazy pp-coll"
-                          src={AuthorImage}
-                          alt="Monica Lucas"
-                        />
-                      </Link>
-
-                      <i className="fa fa-check"></i>
-                    </div>
-
-                    {/* Collection Information */}
-                    <div className="nft_coll_info">
-                      <Link to="/explore">
-                        <h4>Pinky Ocean</h4>
-                      </Link>
-
-                      <span>ERC-192</span>
+                <div className="nft__item_wrap">
+                  <div className="nft__item_extra">
+                    <div className="nft__item_buttons">
+                      <button>Buy Now</button>
+                      <div className="nft__item_share">
+                        <h4>Share</h4>
+                        <a href="" target="_blank" rel="noreferrer">
+                          <i className="fa fa-facebook fa-lg"></i>
+                        </a>
+                        <a href="" target="_blank" rel="noreferrer">
+                          <i className="fa fa-twitter fa-lg"></i>
+                        </a>
+                        <a href="">
+                          <i className="fa fa-envelope fa-lg"></i>
+                        </a>
+                      </div>
                     </div>
                   </div>
+
+                  <Link to="/item-details">
+                    <img
+                      src={nftImage}
+                      className="lazy nft__item_preview"
+                      alt=""
+                    />
+                  </Link>
                 </div>
-              ))}
-            </Slider>
-          </div>
+                <div className="nft__item_info">
+                  <Link to="/item-details">
+                    <h4>Pinky Ocean</h4>
+                  </Link>
+                  <div className="nft__item_price">3.08 ETH</div>
+                  <div className="nft__item_like">
+                    <i className="fa fa-heart"></i>
+                    <span>69</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
   );
 };
 
-export default HotCollections;
+export default NewItems;
